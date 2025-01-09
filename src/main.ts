d6d63c82c5eb02a009b0c3a8af8067921ecfe374
spiridonov <annie.skywalker@yandex.ru>
2025-01-10 01:39:25 +0300

    первичная установка eslint

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(sum(1, 2));
+console.log(sum(1, 2));
