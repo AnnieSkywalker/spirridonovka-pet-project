d2c8840af18cf62edc9a5b7259357d268708f206
spiridonov <annie.skywalker@yandex.ru>
2025-01-29 12:13:15 +0300

    корректировка

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Inspect from 'vite-plugin-inspect'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import Inspect from 'vite-plugin-inspect';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Inspect(),
-],
-})
+    plugins: [vue(), Inspect()],
+    server: {
+        port: 80, // Указываем порт 80
+        host: 'localhost', // Опционально: указываем хост
+    },
+});
