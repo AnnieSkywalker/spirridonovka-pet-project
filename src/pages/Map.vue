f2fc48de46906fcf9715625d949e9dabe6f76396
spiridonov <annie.skywalker@yandex.ru>
2025-02-11 01:06:47 +0300

    корректировки по картам

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="map__wrapper">
+        <h2 class="section-title">Карта (mapbox)</h2>
         <div
             ref="mapContainer"
             class="map-container"
@@ -17,6 +18,7 @@
     import { onBeforeUnmount, onMounted, ref } from 'vue';
 
     // Конфигурация карты
+    const accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
     const mapStyle = 'mapbox://styles/mapbox/streets-v11';
     const center = [30.315591171025375, 59.95024400191426];
@@ -28,6 +30,9 @@
     // Экземпляр карты
     let map = ref(null);
 
+    // Текущая метка
+    let currentMarker = ref(null);
+
     const cities = {
         home: { center: [30.46191281512, 59.91029599127842], zoom: 16 },
         ladogaRoadOfLife: { center: [31.067184825617886, 60.0815295995626], zoom: 13 },
@@ -36,12 +41,27 @@
     const flyToCity = city => {
         if (!map.value || !cities[city]) return;
 
+        // Удаляем предыдущую метку, если она есть
+        if (currentMarker.value) {
+            currentMarker.value.remove();
+            currentMarker.value = null;
+        }
+
         const { center, zoom } = cities[city];
         map.value.flyTo({
             center, // Координаты города
             zoom, // Уровень масштабирования
             essential: true, // Анимация обязательна
         });
+
+        // Добавляем новую метку
+        currentMarker.value = new mapboxgl.Marker({ color: '#FF0000' }) // Красная метка
+            .setLngLat(center) // Устанавливаем координаты
+            .addTo(map.value); // Добавляем на карту
+
+        // Добавляем всплывающее окно (popup)
+        const popup = new mapboxgl.Popup({ offset: 25 }).setText(city);
+        currentMarker.value.setPopup(popup);
     };
 
     onMounted(() => {
@@ -57,11 +77,11 @@
 
         // Ждем, пока карта загрузится
         map.value.on('load', () => {
-            // Устанавливаем эффект "глобуса"
-            map.value.setFog({}); // Добавляем туман для реалистичности
-
             // Добавляем панель управления (зум и вращение)
             map.value.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+            addMarker(cities.home.center, 'Home');
+            addMarker(cities.ladogaRoadOfLife.center, 'Ladoga');
         });
     });
 
@@ -80,15 +100,15 @@
     .map-container {
         width: 100%;
         height: 50vh;
+        filter: var(--darkening-map);
     }
 
     .controls {
-        position: absolute;
-        bottom: 20px;
-        left: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 5px;
         z-index: 1;
-        background: rgba(255, 255, 255, 0.8);
-        padding: 10px;
+        padding: 5px 7px;
         border-radius: 5px;
     }
 
@@ -98,13 +118,13 @@
         padding: 8px 12px;
         font-size: 14px;
         cursor: pointer;
-        background-color: #4caf50;
-        color: white;
-        border: none;
-        border-radius: 4px;
+        color: var(--primary);
+        border: 1px solid var(--primary);
+        background: none;
+        border-radius: 8px;
     }
 
     button:hover {
-        background-color: #45a049;
+        opacity: 0.6;
     }
 </style>
